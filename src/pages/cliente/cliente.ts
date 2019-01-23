9dd357548291de74e83c2dd33d89bf5aae901d07
Vinicius Duarte Galdino <vduarteg@everis.com>
2019-01-23 17:37:30 -0200

    usuario logado

--- src/pages/cliente/cliente.ts
+++ src/pages/cliente/cliente.ts
@@ -1,15 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, Item } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ClienteService } from '../../services/domain/cliente.service';
 import { ClienteDTO } from '../../models/cliente.dto';
-import { Observable } from 'rxjs';
-
-/**
- * Generated class for the ClientePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
