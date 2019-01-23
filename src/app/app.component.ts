5f12b01e2b6561e495291ef94e49d4652dc18171
Vinicius Duarte Galdino <vduarteg@everis.com>
2019-01-23 19:28:20 -0200

    criado pagina de signup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,7 +22,8 @@ export class MyApp {
     this.pages = [
       { title: 'Profile', component: 'ProfilePage' },
       { title: 'Categorias', component: 'CategoriasPage' },
-      { title: 'Logout', component: '' }
+      { title: 'Criar perfil', component: 'SignupPage' },
+      { title: 'Logout', component: '' },
     ];
 
   }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -50,4 +50,8 @@ creds : CredenciaisDTO = {
     },
     error=>{});
   }
+
+  signup(){
+    this.navCtrl.push('SignupPage');
+  }
 }
